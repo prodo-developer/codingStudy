0374d31fa915668f92d0bbc00a2d48bbc2986673
prodo-developer <wjdals300@gmail.com>
2022-02-10 23:40:43 +0900

    feat : EmailService 공통 모듈 추가

---
+++ src/main/java/com/fastJava8/service/EmailService.java
@@ -0,0 +1,14 @@
+package com.fastJava8.service;
+
+import com.fastJava8.model.User;
+
+public class EmailService {
+
+    public void sendPlayWithFriendsEmail(User user) {
+        user.getEmailAddress().ifPresent(email -> System.out.println("sending 'Play With Friends' email to " + email));
+    }
+
+    public void sendMakeMoreFriendsEmail(User user) {
+        user.getEmailAddress().ifPresent(email -> System.out.println("sending 'Make More Friends' email to " + email));
+    }
+}

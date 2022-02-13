d6776b4c1d9bd3b00fda74ec9ecec57727528e52
prodo-developer <wjdals300@gmail.com>
2022-02-14 01:06:28 +0900

    refactor : 이메일 찾기 기능 추가

--- src/main/java/com/fastJava8/service/EmailService.java
+++ src/main/java/com/fastJava8/service/EmailService.java
@@ -11,4 +11,9 @@ public class EmailService {
     public void sendMakeMoreFriendsEmail(User user) {
         user.getEmailAddress().ifPresent(email -> System.out.println("sending 'Make More Friends' email to " + email));
     }
+
+    public void sendVerifyYourEmailEmail(User user) {
+        user.getEmailAddress().ifPresent(email ->
+                System.out.println("Sending 'Verify Your Email' email to " + email));
+    }
 }

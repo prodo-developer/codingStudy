4324026bba81dde6a8e6a51e1ada1130a284696b
prodo-developer <wjdals300@gmail.com>
2022-05-16 23:36:44 +0900

    refactor :파일 암호화 테스트

--- src/main/java/com/rsaStudy/FileTest.java
+++ src/main/java/com/rsaStudy/FileTest.java
@@ -1,18 +1,28 @@
 package com.rsaStudy;
 
-import java.io.File;
+import static com.rsaStudy.DesUtil.outFilePath;
 
 public class FileTest {
 
     public static void main(String[] args) throws Exception {
 
-        String key = "0123456789abcdefghij0123456789ab";
+//      암호
+        DesUtil desFileEncrypt = new DesUtil();
+        desFileEncrypt.decrypt(outFilePath[0]);
+//      복호
+//        String testFile = "polymorphList.bin";
+//        desFileEncrypt.encrypt(outFilePath[0], testFile);
+
+//      AES
+//      암호화
+//        String key = "Tree53464112sss";
 //        File xmlFile = new File("polymorphList.xml");
 //        File downFile = new File("C:/Programer/imageTest/polymorphLists.xml");
 //        new FileCoder(key).encrypt(xmlFile, downFile);
 
-        File xmlFile = new File("C:/Programer/imageTest/polymorphLists.xml");
-        File downFile = new File("C:/Programer/imageTest/polymorphListses.xml");
-        new FileCoder(key).decrypt(xmlFile, downFile);
+//      복호화
+//      File xmlFile = new File("C:/Programer/imageTest/polymorphLists.xml");
+//      File downFile = new File("C:/Programer/imageTest/polymorphListses.xml");
+//      new FileCoder(key).decrypt(xmlFile, downFile);
     }
 }

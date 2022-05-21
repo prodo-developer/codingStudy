169489756a040928c0c7e348753bd4db59d6664c
prodo-developer <wjdals300@gmail.com>
2022-05-21 18:32:23 +0900

    feat : 세금계산에 필요한 공통모듈 샘플 작성

---
+++ src/main/java/com/fastJava8/priceprocessor/OrderLineAggregationPriceProcessor.java
@@ -0,0 +1,18 @@
+package com.fastJava8.priceprocessor;
+
+import com.fastJava8.model.Order;
+import com.fastJava8.model.OrderLine;
+
+import java.math.BigDecimal;
+import java.util.function.Function;
+
+public class OrderLineAggregationPriceProcessor implements Function<Order, Order> {
+
+    @Override
+    public Order apply(Order order) {
+        return order.setAmount(order.getOrderLines().stream()
+                .map(OrderLine::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add));
+    }
+
+}
